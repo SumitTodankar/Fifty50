00f157c6ad967d0ab6dce3b2b1b785eec5e83dc3
sumit_todankar <todankarsumit97@gmail.com>
2024-07-02 21:43:11 +0530

    Progress steps done

--- 02-progressSteps/script.js
+++ 02-progressSteps/script.js
@@ -1,4 +1,38 @@
-const progress = document.getElementById('progress');
 const circles = document.querySelectorAll('.step');
+const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
+
+let counter = 1;
+
+next.addEventListener("click",() => {
+    counter++;
+    if(counter > circles.length){
+        counter = circles.length;
+    }
+    update();
+})
+
+prev.addEventListener("click",() => {
+    counter--;
+    if(counter < 1){
+        counter = 1;
+    }
+    update();
+})
+
+function update(){
+    circles.forEach((item,indx) => {
+        if(counter > indx){
+            item.classList.add('active');
+        }else{
+            item.classList.remove('active');
+        }
+    })
+
+    const activeCircles = document.querySelectorAll('.step.active');
+
+    console.log(activeCircles.length ,circles.length);
+
+    progress.style.width =  ((activeCircles.length - 1) / (circles.length - 1)) * 100 + "%"
+}
